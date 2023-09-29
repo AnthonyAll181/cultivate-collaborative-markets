a27132f5b4364aec6b6ddd511870d6f5a367b39c
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-09-29 08:02:46 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,27 +1,52 @@
-
-    return data
-
-def generate_random_data():
-    main()
-def main():
-    data = generate_random_data()
 if __name__ == "__main__":
-
-import random
-    data = [random.randint(1, 100) for _ in range(10)]
+    main()
     for item in data:
 
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+
+def main():
+
+import random
+    data = generate_random_data()
         print(f"Random Number: {item}")
+    return data    return data
+
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+def generate_random_data():
+    data = generate_random_data()
+import random
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
 
     main()
+    for item in data:
+
+    main()
+
 def generate_random_data():
     data = [random.randint(1, 100) for _ in range(10)]
 
-        print(f"Random Number: {item}")
-    for item in data:
+def main():
+
 import random
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+    return data
+if __name__ == "__main__":
+    for item in data:    for item in data:
+import random
+def generate_random_data():
     return data
 
 if __name__ == "__main__":
-def main():
+    main()
     data = generate_random_data()
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+
