e326780e62cd169777f18cbb43ba44dec90e21fd
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-10-02 17:08:59 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,27 +1,40 @@
-        print(f"Random Number: {item}")
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
     for item in data:
-    data = generate_random_data()
+    return data
+
+
+        print(f"Random Number: {item}")
+import random
+def main():
     main()
 
+if __name__ == "__main__":
 
+    data = generate_random_data()def generate_random_data():
+def main():
     data = [random.randint(1, 100) for _ in range(10)]
 
-
-def generate_random_data():
-def main():
-import random
-if __name__ == "__main__":
-    return data    data = [random.randint(1, 100) for _ in range(10)]
 
 
     return data
     for item in data:
 
-def main():
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    data = generate_random_data()
     main()
+import randomdef main():
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+    data = generate_random_data()
+
+    main()
+
+    return data
+import random
+def generate_random_data():
+
         print(f"Random Number: {item}")
 
-import random
-    data = generate_random_data()
-if __name__ == "__main__":
-def generate_random_data():
+    for item in data:
