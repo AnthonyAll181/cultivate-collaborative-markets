9c06eff2836924f676c4b659f88d201d8634f5c0
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-10-04 05:11:49 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,14 +1,39 @@
+
+    data = [random.randint(1, 100) for _ in range(10)]
     return data
         print(f"Random Number: {item}")
-    data = generate_random_data()
+    for item in data:
+def main():
+import random
+def generate_random_data():
 
 if __name__ == "__main__":
     main()
-import random
+
+    data = generate_random_data()
+public class RandomDataGenerator {
+
+        }
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+        }
+        int[] data = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+}
+        for (int item : data) {
+    }if __name__ == "__main__":
 def generate_random_data():
-def main():
+    for item in data:
 
 
     data = [random.randint(1, 100) for _ in range(10)]
 
-    for item in data:
+    data = generate_random_data()
+    return data
+        print(f"Random Number: {item}")
+def main():
+
+import random
+    main()
